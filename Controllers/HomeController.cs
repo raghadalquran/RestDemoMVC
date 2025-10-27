e5976d2b60e8ce549dee187525687c1fa300047e
AhmadBakeer <AhmadBakeer730@yahoo.com>
2025-10-27 10:27:45 +0300

    test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,10 +25,10 @@ namespace RestDemoMVC.Controllers
             return View();
         }
 
-        //public IActionResult Privacy()
-        //{
-        //    return View();
-        //}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
